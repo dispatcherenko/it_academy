294440a533150f08ed9a19189d3ae21872f43d8f
dispatcherenko <chda.contact@gmail.com>
2024-02-22 17:56:17 +0300

    38

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -347,4 +347,42 @@
             title: "TAGREE digital"
         });
     }
+
+    const jsSelectric = $('select').selectric();
+    if (jsSelectric.length) {
+      jsSelectric.selectric({
+        nativeOnMobile: false
+      });
+    }
+
+    const mobileMask = $('.js-mobileMask');
+    if (mobileMask.length) {
+      mobileMask.mask('+7 (000) 000 00 00', { placeholder: "+7 (___) ___ __ __" });
+    }
+
+    const dateField = $('.js-dateField');
+    if (dateField.length) {
+      const pickerInit = function (pick) {
+        const dateInput = pick.find(".js-dateInput");
+        const dateDay = pick.find(".js-dateDay");
+        const dateMonth = pick.find(".js-dateMonth");
+        const dateYear = pick.find(".js-dateYear");
+        const dateConfig = {
+          autoClose: true,
+          minDate: new Date(),
+          navTitles: {
+            days: "MMMM <i>yyyy</i>"
+          },
+          onSelect: function ({ date }) {
+            dateDay.val(date ? ("0" + date.getDate()).slice(-2) : "");
+            dateMonth.val(date ? ("0" + (date.getMonth() + 1)).slice(-2) : "");
+            dateYear.val(date ? date.getFullYear() : "");
+          }
+        };
+        new AirDatepicker(dateInput[0], dateConfig);
+      };
+      $.each(dateField, function (i) {
+        pickerInit($(this));
+      });
+    }
 })();
